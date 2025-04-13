dc71cef1273dd9c4107ff55937c1da5b54eec107
Ildarc4S <ildarc4S@gmail.com>
2025-04-13 21:12:44 +0300

    test inheritance

--- src/3_8_inheritance.cpp
+++ src/3_8_inheritance.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-struct Base() {
+struct Base {
   int x;
   void f() {}
 };
 
-struct Derived {
+struct Derived : Base {
   int y;
   void g() {}
 };
 
 int main() {
   Derived d;
-  d.x;
+  std::cout << d.x;
   return 0;
 }
